c1cffdee848692d1777754384292dd71fafa04ba
Felipe Urra <87881375+felipeUrra@users.noreply.github.com>
2025-04-02 12:00:09 +0300

    Create arrayWrapper.h

---
+++ solutions/arrayWrapper.h
@@ -0,0 +1,33 @@
+#pragma once
+
+enum class ArrId {ARR1 = 1, ARR2 = 2};
+
+class ArrayWrapper
+{
+private:
+    int* arr1;
+    int* arr2;
+    int size1;
+    int size2;
+
+public:
+    ArrayWrapper();
+
+    //Big four
+    ArrayWrapper(const int*, const int*, int, int);
+    ArrayWrapper(const ArrayWrapper&);
+    ArrayWrapper& operator=(const ArrayWrapper&);
+    ~ArrayWrapper();
+
+    int* getArr(ArrId) const;
+    void setArr(ArrId, const int*, int);
+    
+    void addElement(ArrId, int);
+    void eliminateElement(ArrId, int);
+
+    int* rest();
+    int* sum();
+
+    void copyFrom(const ArrayWrapper&);
+    void free();
+};
